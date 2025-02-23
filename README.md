b8219496ab713a87923c6c5cb1d8f5193b0c08bd
fir3yice (ceo) <126239345+fir3yice@users.noreply.github.com>
2025-02-23 17:15:58 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# Glove-Based-Pointing-Device
+# Glove-Based Pointing Device Using MPU6050 (Accelerometer and Gyroscope-based Sensor)
+
+#### Device Overview
+The proposed device is a microcontroller-based prototype, leveraging a sensor as its input device. Affixed to a glove, it relies on hand movements as its main source of input. This also allows the presenter to have free use of both of their hands for props or other actions during presentations. Attached is the [link](https://drive.google.com/file/d/1l2kTV054JVqjq5POpGmrDK0Weq7EVZKx/view?usp=sharing) to a demonstration of the working prototype.
+### Firmware
+The firmware is implemented in Arduino that uses processed motion input from the MPU6050 sensor to control the mouse cursor on a connected device. The Inter-Integrated Circuit (I2C) Device Library is the main library used for this implementation. It is a library that simplifies communication between devices that use the I2C protocol, a serial communication and bus interface connection protocol to connect multiple devices through a shared bus. This is referenced from [I2C Dev Library](https://github.com/jrowberg) of Jeff Rowberg. Two versions of the prototype device were proposed, one using a 0.5 sensitivity factor with a 16 X-threshold and an 8 Y-threshold value, and the other using a 0.625 sensitivity with an 8 X-threshold and a 4 Y-threshold
+
+
+### Guidelines when Running the Program
+1. Open gyro_mouse folder using the Arduino IDE.
+2. Import the mouse library using the built-in library manager. 
+3. Upload to Arduino.
+*Note: Ensure that the correct COM port and board are selected.*
+
+### Hardware Used
+To build the prototype, hardware components include an Arduino Pro Micro, an MPU-6050 sensor, breadboards, and male-to-male cables. All components are attached to the microprocessor as shown in the circuit schematic diagram below.
+[![Circuit-Schematic.png](https://i.postimg.cc/QMVwpGgc/Circuit-Schematic.png)](https://postimg.cc/06RVs4BN)
+
+### Testing Interface
+To assess the effectiveness of the proposed prototype, a navigation task adapted from a hierarchical menu is delivered through an HTML page depicting an abstract display similar to the Figure below. This may be loaded through the **navigation_task.html** file.
+[![Abstract-UI.png](https://i.postimg.cc/XJy3L0gR/Abstract-UI.png)](https://postimg.cc/TLGZ2FF0)
+The navigation task is performed by hovering the cursor over the highlighted buttons in the interface. This HTML page is shown to the participants through a large TV display. The participant then positioned themselves at their preferred distance in the center front of the TV display, with a maximum distance of 3 meters away from the screen. The experiment is composed of four trials with each trial having four randomly placed targets. The movement time for the four targets of each trial is then recorded.
+
+### Analysis Conducted
+A T-test was calculated to determine if there was a significant difference among the performances of the three devices involved in the study as gathered through the testing done. A comparative visual examination of the performance metrics across the three groups was also conducted through data visualization techniques, such as box plots, histograms, and line graphs, to provide a comprehensive understanding of the differences in performance among the control group (original interface) and the two versions of the proposed prototype. This was all done through a series of **Jupyter notebooks** located in the folder **Jupyter Notebooks for Analysis**. 
---
+++ gyro_mouse/gyro_mouse/gyro_mouse.ino
@@ -0,0 +1,205 @@
+/* ============================================
+I2Cdev device library code is placed under the MIT license
+Copyright (c) 2012 Jeff Rowberg
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+===============================================
+*/
+
+#include "I2Cdev.h"
+
+#include "MPU6050_6Axis_MotionApps612.h"
+
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    #include "Wire.h"
+#endif
+#include <Mouse.h>
+MPU6050 mpu;
+
+#define OUTPUT_READABLE_YAWPITCHROLL
+
+//#define OUTPUT_READABLE_WORLDACCEL
+
+
+#define INTERRUPT_PIN 2  // use pin 2 on Arduino Uno & most boards
+
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+// packet structure for InvenSense teapot demo
+uint8_t teapotPacket[14] = { '$', 0x02, 0,0, 0,0, 0,0, 0,0, 0x00, 0x00, '\r', '\n' };
+
+
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void dmpDataReady() {
+    mpuInterrupt = true;
+}
+
+// ================================================================
+// ===                      INITIAL SETUP                       ===
+// ================================================================
+
+void setup() {
+    // join I2C bus (I2Cdev library doesn't do this automatically)
+    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+        Wire.begin();
+        Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+        Fastwire::setup(400, true);
+    #endif
+
+    // initialize serial communication
+    Serial.begin(115200);
+    //while (!Serial); // wait for Leonardo enumeration, others continue immediately
+
+    // initialize device
+    Serial.println(F("Initializing Mouse..."));
+    Mouse.begin();
+
+    Serial.println(F("Initializing I2C devices..."));
+    mpu.initialize();
+
+    // verify connection
+    Serial.println(F("Testing device connections..."));
+    Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+
+    // load and configure the DMP
+    Serial.println(F("Initializing DMP..."));
+    devStatus = mpu.dmpInitialize();
+
+    // supply your own gyro offsets here, scaled for min sensitivity
+    mpu.setXGyroOffset(220);
+    mpu.setYGyroOffset(76);
+    mpu.setZGyroOffset(-85);
+    mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+
+    // make sure it worked (returns 0 if so)
+    if (devStatus == 0) {
+        // Calibration Time: generate offsets and calibrate our MPU6050
+        mpu.CalibrateAccel(6);
+        mpu.CalibrateGyro(6);
+        mpu.PrintActiveOffsets();
+        // turn on the DMP, now that it's ready
+        Serial.println(F("Enabling DMP..."));
+        mpu.setDMPEnabled(true);
+
+        // enable Arduino interrupt detection
+        Serial.print(F("Enabling interrupt detection (Arduino external interrupt "));
+        Serial.print(digitalPinToInterrupt(INTERRUPT_PIN));
+        Serial.println(F(")..."));
+        attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+        mpuIntStatus = mpu.getIntStatus();
+
+        // set our DMP Ready flag so the main loop() function knows it's okay to use it
+        Serial.println(F("DMP ready! Waiting for first interrupt..."));
+        dmpReady = true;
+
+        // get expected DMP packet size for later comparison
+        packetSize = mpu.dmpGetFIFOPacketSize();
+    } else {
+        // ERROR!
+        // 1 = initial memory load failed
+        // 2 = DMP configuration updates failed
+        // (if it's going to break, usually the code will be 1)
+        Serial.print(F("DMP Initialization failed (code "));
+        Serial.print(devStatus);
+        Serial.println(F(")"));
+    }
+}
+
+
+float sensitivity = 0.5;
+float threshold = 8.0;
+
+float linearFunction(float x) {
+    if (x == 0) P\{return 0;}
+    return 0.75*x + 0.25;
+}
+
+void moveMouse(float x, float y) {
+    if (x < threshold && x > - threshold) {x = 0;}
+    else if (x > threshold) {x = x - threshold;}
+    else if (x < -threshold){x = x + threshold;}
+    if (y < threshold && y > - threshold) {y = 0;}
+    else if (y > threshold / 2) {y = y - threshold / 2;}
+    else if (y < -threshold / 2){y = y + threshold / 2;}
+    float converted = 0.75*sensitivity + 0.25;
+    Mouse.move(x * converted, y * converted);
+}
+
+
+
+// ================================================================
+// ===                    MAIN PROGRAM LOOP                     ===
+// ================================================================
+
+void loop() {
+    // if programming failed, don't try to do anything
+    if (!dmpReady) return;
+    // read a packet from FIFO
+    if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer)) { // Get the Latest packet 
+        
+
+        #ifdef OUTPUT_READABLE_YAWPITCHROLL
+            // display Euler angles in degrees
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+            mpu.dmpGetGravity(&gravity, &q);
+            mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+            Serial.print("ypr\t");
+            Serial.print(ypr[0] * 180/M_PI);
+            Serial.print("\t");
+            Serial.print(ypr[1] * 180/M_PI);
+            Serial.print("\t");
+            Serial.println(ypr[2] * 180/M_PI);
+            if (ypr[1]* 180/M_PI < 3*threshold and ypr[1] * 180/M_PI> -3*threshold) {
+              moveMouse(ypr[0] * 180/M_PI, ypr[2] * 180/M_PI);
+            }
+        #endif
+
+        #ifdef OUTPUT_READABLE_WORLDACCEL
+            // display initial world-frame acceleration, adjusted to remove gravity
+            // and rotated based on known orientation from quaternion
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+            mpu.dmpGetAccel(&aa, fifoBuffer);
+            mpu.dmpGetGravity(&gravity, &q);
+            mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+            mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
+            Serial.print("aworld\t");
+            Serial.print(aaWorld.x);
+            Serial.print("\t");
+            Serial.print(aaWorld.y);
+            Serial.print("\t");
+            Serial.println(aaWorld.z);
+        #endif
+    }
+}
